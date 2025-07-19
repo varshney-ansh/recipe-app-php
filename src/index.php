3c9e9ec57030eef7be00e3f77acb439eb9ce5e61
Ansh Varshney <anshvarshney566@gmail.com>
2025-07-19 00:48:50 -0700

    fonts and tailwind added

---
+++ src/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TasteAI | Powered by Slew</title>
+    <link rel="stylesheet" href="./output.css">
+</head>
+<body>
+    <h1 class="font-bold text-center text-3xl font-qurova">tasteai</h1>
+</body>
+</html>
