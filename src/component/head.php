3859a1f3bf81d8a51cd7b480724a2c37c45ff6b0
Ansh Varshney <anshvarshney566@gmail.com>
2025-07-22 05:46:43 -0700

    ..

---
+++ src/component/head.php
@@ -0,0 +1,15 @@
+<?php
+
+class Head{
+    public static function render(String $title = "TasteAI | Powered by Slew"): void {
+        ?>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title><?php echo $title; ?></title>
+            <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+            rel="stylesheet">
+        <?php
+    }
+}
+
+?>
